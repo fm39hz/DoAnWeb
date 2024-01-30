3ad15271ef2228dbbf49125d264c28e60f6d202b
fm39hz <hitpoint2k3@gmail.com>
2024-01-31 00:34:13 +0700

    feat: added exist checking for db

--- WebData/run-initialization.sh
+++ WebData/run-initialization.sh
@@ -1,1 +1,13 @@
-sleep 90s && echo 'Creating database schema' && /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P @VeryComplexPassword1thTime -i /usr/src/app/CreateDB.sql && echo 'Created schema, starting import data' && /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P @VeryComplexPassword1thTime -i /usr/src/app/WebData.sql
+alias SQL_DIRECTORY='/var/opt/mssql/'
+alias SQL_CMD='/opt/mssql-tools/bin/sqlcmd'
+sleep 90s
+if [ -d "SQL_DIRECTORY" ] || [ -z "(ls -A SQL_DIRECTORY)" ]; then
+    echo "Starting to create database."
+    SQL_CMD -S localhost -U sa -P @VeryComplexPassword1thTime -i /usr/src/app/CreateDB.sql
+    echo "Starting to insert data into database."
+    SQL_CMD -S localhost -U sa -P @VeryComplexPassword1thTime -i /usr/src/app/InsertData.sql
+    rm -f ./*.sql
+    echo "Initialized database."
+else
+	echo "Database exist, will not insert data."
+fi
